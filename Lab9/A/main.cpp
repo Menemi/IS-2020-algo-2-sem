098fa04b510dd24e1205d5b33f21bf7a4b18d3bd
DAH9 <danila.titov1210@yandex.ru>
2021-03-03 20:39:59 +0300

    Initial commit

---
+++ Lab9/A/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin("topsort.in");
+    ofstream fout("topsort.in");
+
+    int n, m;
+    fin >> n >> m;
+}
