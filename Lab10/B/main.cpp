d71dcc5960030993159bc63cd2f8c5f7b13dd0c4
DAH9 <danila.titov1210@yandex.ru>
2021-03-21 01:24:03 +0300

    Initial commit

---
+++ Lab10/B/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream fin("spantree.in");
+    ofstream fout("spantree.out");
+    int n;
+
+    fin >> n;
+}
