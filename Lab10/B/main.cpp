ea9f75c3f3c7ad1a1655bbbc49f26a15cbce7248
DAH9 <danila.titov1210@yandex.ru>
2021-03-25 19:50:57 +0300

    fixed B

--- Lab10/B/main.cpp
+++ Lab10/B/main.cpp
@@ -1,13 +1,69 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
+
+//Can u help me, Mr. Goose?
+//░░░░▄▀▀▀▄░░░░░░░░
+//▄██▀░◐░░░▌░░░░░░░
+//░░░▌░░░░░▐░░░░░░░
+//░░░▐░░░░░▐░░░░░░░
+//░░░▌░░░░░▐▄▄░░░░░
+//░░░▌░░░░▄▀▒▒▀▀▀▀▄
+//░░▐░░░░▐▒▒▒▒▒▒▒▒▀▀▄
+//░░▐░░░░▐▄▒▒▒▒▒▒▒▒▒▒▀▄
+//░░░▀▄░░░░▀▄▒▒▒▒▒▒▒▒▒▒▀
+//░░░░░▀▄▄▄▄▄█▄▄▄▄▄▄▄▄▄▄
+//░░░░░░░░░░▌▌░▌▌░░░░░
+//░░░░░░░░░░▌▌░▌▌░░░░░
+//░░░░░░░░▄▄▌▌▄▌▌░░░░░
+//YEP YEP YEP
 
 int main() {
     ifstream fin("spantree.in");
     ofstream fout("spantree.out");
-    int n;
+    fout.precision(10);
 
+    int n;
     fin >> n;
+
+    const int INF = INT_MAX;
+    vector<vector<int>> graph(n, vector<int>(n));
+    vector<int> minE(n, INF), x(n), y(n);
+    vector<bool> used(n);
+    double result = 0;
+
+    for (int i = 0; i < n; i++) {
+        fin >> x[i] >> y[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            graph[i][j] = (x[i] - x[j]) * (x[i] - x[j]) +
+                          (y[i] - y[j]) * (y[i] - y[j]);
+        }
+    }
+
+    minE[0] = 0;
+    for (int i = 0; i < n; i++) {
+        int v = -1;
+
+        for (int j = 0; j < n; j++)
+            if (!used[j] && (v == -1 || minE[j] < minE[v]))
+                v = j;
+
+        used[v] = true;
+        for (int j = 0; j < n; j++) {
+            if (!used[j] && graph[v][j] < minE[j] && v != j)
+                minE[j] = graph[v][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        result += sqrt(minE[i]);
+    }
+
+    fout << result;
 }
