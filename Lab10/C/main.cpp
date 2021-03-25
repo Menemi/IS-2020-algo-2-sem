59be129f82731b01a4d124340bf97910731020a7
DAH9 <danila.titov1210@yandex.ru>
2021-03-25 23:13:50 +0300

    fixed C

--- Lab10/C/main.cpp
+++ Lab10/C/main.cpp
@@ -5,16 +5,33 @@
 
 using namespace std;
 
+vector<int> treeID;
+
+int DSUget(int v) {
+    if (v == treeID[v])
+        return v;
+    else
+        treeID[v] = DSUget(treeID[v]);
+}
+
+void DSUunite(int firstT, int secondT) {
+    firstT = DSUget(firstT);
+    secondT = DSUget(secondT);
+
+    if (firstT != secondT)
+        treeID[firstT] = secondT;
+}
+
 int main() {
     ifstream fin("spantree3.in");
     ofstream fout("spantree3.out");
 
     int n, m;
-    double result = 0;
+    long long result = 0;
     fin >> n >> m;
-    vector<pair<long long int, pair<int, int>>> graph;
-    vector<pair<int, int>> res;
-    vector<int> treeID(n);
+
+    vector<pair<long long, pair<long long, long long>>> graph;
+    treeID.resize(n);
 
     for (int i = 0; i < m; i++) {
         int top1, top2, weight;
@@ -24,22 +41,17 @@ int main() {
 
     sort(graph.begin(), graph.end());
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; ++i)
         treeID[i] = i;
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; ++i) {
         int firstT = graph[i].second.first;
         int secondT = graph[i].second.second;
         int tempWeight = graph[i].first;
 
-        if (treeID[firstT] != treeID[secondT]) {
+        if (DSUget(firstT) != DSUget(secondT)) {
             result += tempWeight;
-
-            int oldID = treeID[secondT];
-            int newID = treeID[firstT];
-            for (int j = 0; j < n; j++)
-                if (treeID[j] == oldID)
-                    treeID[j] = newID;
+            DSUunite(firstT, secondT);
         }
     }
 
